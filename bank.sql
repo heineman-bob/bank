028ba428955cb540491e221a50bb79c5bd38a0b9
slmd2k3 <heineman.bob@gmail.com>
2014-07-19 21:06:21 -0700

    Sql config
    Create database script
    Configure app to use new user

--- bank.sql
+++ bank.sql
@@ -1,9 +1,27 @@
+#Flush the database if it exists
+DROP DATABASE `bank`;
+
+
+#Create User for banking app
+#A Note on Security here.  This authentication and password are just here for ease of development
+#  in an enterprise setting something more robust would be used to secure the database and all privileges would 
+#  most likely not be granted.
+CREATE USER 'bankingApp'@'localhost' IDENTIFIED BY 'bankingApp';
+#Grant access
+GRANT ALL PRIVILEGES ON bank.* TO 'bankingApp'@'localhost' WITH GRANT OPTION;
+
+#Create Bank Schema
 CREATE SCHEMA `bank`;
 
+#Create the User table
 CREATE TABLE `bank`.`user` (
     `id` BIGINT NOT NULL AUTO_INCREMENT,
     `firstName` VARCHAR(45) NULL,
     `lastName` VARCHAR(45) NULL,
     PRIMARY KEY (`id`));
 
+#Insert sandbox data
 INSERT INTO `bank`.`user` (`id`, `firstName`, `lastName`) VALUES ('1', 'Bob', 'Heineman');
+INSERT INTO `bank`.`user` (`id`, `firstName`, `lastName`) VALUES ('2', 'Jen', 'McCord');
+INSERT INTO `bank`.`user` (`id`, `firstName`, `lastName`) VALUES ('3', 'Veronica', 'Heineman');
+INSERT INTO `bank`.`user` (`id`, `firstName`, `lastName`) VALUES ('4', 'Mark', 'Heineman');
